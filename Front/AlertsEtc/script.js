f0bd0a161800c3499abfad71c7aaece4ce769e4e
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-12 13:27:20 -0300

    Aula

---
+++ Front/AlertsEtc/script.js
@@ -0,0 +1,21 @@
+// console.log("Olar");
+// alert("Olar");
+// let nome = prompt("Seu nome: ")
+// console.log("Seu nome é ",nome);
+// let confirmar = confirm("Vc é magu?")
+// console.log("É magu", nome,"?",confirmar);
+
+//// 1
+// let nome = prompt("Qual o seu nome?")
+// alert(`Olá ${nome}`);
+
+//// 2
+// let materia = prompt("Qual a sua matéria favorita?")
+// alert(`A matéria ${materia} realmente é ótima!`)
+
+//// 3
+//prompt("Qual a data de hoje?")
+
+//// 4
+//confirm("Está preparado para o segundo semestre de desenvolvimento de sistemas?")
+//alert(`${}`)
--- Front/Aula1/script.js
+++
@@ -1,6 +0,0 @@
-console.log("Olar");
-alert("Olar");
-let nome = prompt("Seu nome: ")
-console.log("Seu nome é ",nome);
-let confirmar = confirm("Vc é magu?")
-console.log("É magu", nome,"?",confirmar);
