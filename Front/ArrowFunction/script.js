c5e9bd1c24ff3997b1b2dc72016254d366077da4
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-12 16:10:05 -0300

    Aula

---
+++ Front/ArrowFunction/script.js
@@ -0,0 +1,10 @@
+// let sub = (n1,n2) =>{
+//     return n1 - n2;
+// }
+
+// OU //
+
+let sub = (n1,n2) => n1 - n2;
+
+console.log(sub(1,2));
+console.log(typeof(sub(1,2)));
--- Front/Funcao/script.js
+++ Front/Funcao/script.js
@@ -2,3 +2,13 @@ function helo(n1,n2){
     console.log(n1 + n2);
 }
 
+helo(3,9);
+
+
+//Funcao anonima
+
+let soma = function(n1,n2){
+    return n1 + n2;
+}
+
+console.log(soma(3,9));
