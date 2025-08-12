9916efda9835e0f3ffb65cb275e8906211524d68
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-12 16:50:09 -0300

    Aula

--- Front/Aulas/DOM/script2.js
+++
@@ -1,5 +0,0 @@
-let btn = document.querySelector("button");
-
-function random(num){
-    return Math.floor(Math.random() * (num * 1));
-}
--- Front/Aulas/DOM/scriptInfinito.js
+++ Front/Aulas/DOM/scriptInfinito.js
@@ -1,10 +1,21 @@
-let btn = document.querySelector("button");
-
 function random(num){
     return Math.floor(Math.random() * (num + 1));
 }
 
-btn.onclick = function (){
+let infinito = function (){
     let cor = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
     document.body.style.backgroundColor = cor;
+}
+
+// for(let i = 0; i < 10; i++){
+//     setTimeout(infinito, 1000);
+// }
+
+
+let i = 0;
+
+while (i < 10){
+    i++;
+    infinito();
+    setTimeout(1000);
 }
