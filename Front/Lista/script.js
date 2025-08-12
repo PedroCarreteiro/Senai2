c9879ccab3f179f710afc712b0ea1f7c0bee747a
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-12 15:57:10 -0300

    Aula

---
+++ Front/Lista/script.js
@@ -0,0 +1,8 @@
+//lista.toString()
+//lista.slice()
+//lista.join(",") - separar a lista com , no meio da string
+//lista.split(",") - criar uma lista com o string com a separação por , 
+//lista.push() - adicionar item
+//lista.pop() - retirar item
+//lista.unshift() - adicionar item no começo
+//lista.shift() - retirar o primeiro                                                          
