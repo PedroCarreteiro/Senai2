9754b8ba12783cd77b78ae7c89ef96b697855a76
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-14 13:49:37 -0300

    Teste no Vue

--- Front/Projetos/bingoVue/src/main.js
+++ Front/Projetos/bingoVue/src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import Teste from './Teste.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+createApp(Teste).mount('#teste')
